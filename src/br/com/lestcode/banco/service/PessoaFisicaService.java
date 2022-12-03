05e9eaf49191df963a335fbbad6ac9e6690938c1
mirilianc <mirilianc@hotmail.com>
2022-12-03 09:35:38 -0300

    commit mirilian

--- src/br/com/lestcode/banco/service/PessoaFisicaService.java
+++
@@ -1,9 +0,0 @@
-package br.com.lestcode.banco.service;
-
-public class PessoaFisicaService {
-
-    public boolean validarCpf() {
-        //validar cpf
-        return true;
-    }
-}
--- src/br/com/lestcode/banco/service/PessoaJuridicaService.java
+++
@@ -1,9 +0,0 @@
-package br.com.lestcode.banco.service;
-
-public class PessoaJuridicaService {
-
-    public boolean validarCnpj() {
-        //validar cnpj
-        return true;
-    }
-}
